e6bc65144774d3877dbe95fb0233ca890d9d4a3e
Tara Routray <dev@tararoutray.com>
2023-12-11 15:56:41 +0300

    addnewdiv

---
+++ script.js
@@ -0,0 +1,31 @@
+const fileInput = document.getElementById('fileInput');
+    const imagePreview = document.getElementById('imagePreview');
+
+    fileInput.addEventListener('change', function() {
+      const file = fileInput.files[0];
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        imagePreview.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    });
+
+    function readFile() {
+      const fileInput = document.getElementById('fileInput');
+      const file = fileInput.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+          // Обработка содержимого файла
+          console.log(e.target.result);
+        };
+        reader.readAsText(file);
+      }
+    }
+
+    function writeFile() {
+      const fileData = 'Приветвцуацуаацауц';
+      const blob = new Blob([fileData], { type: 'text/plain;charset=utf-8' });
+      const fileName = 'example.txt';
+      saveAs(blob, fileName);
+    }
